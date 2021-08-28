2888a15abc47c90aa2ea41d1111191f8382df459
Chengwei Li <chengwei.li@towerswatson.com>
2021-08-28 23:55:43 +1000

    Added the dotnet pack properties

--- Xunit.Di/DiXunitTestFrameworkExecutor.cs
+++ Xunit.Di/DiXunitTestFrameworkExecutor.cs
@@ -10,23 +10,21 @@ namespace Xunit.Di
     {
         private readonly IServiceProvider? _serviceProvider;
 
-        public DiXunitTestFrameworkExecutor(IServiceProvider serviceProvider, AssemblyName assemblyName,
-            ISourceInformationProvider sourceInformationProvider, IMessageSink diagnosticMessageSink) : base(
-            assemblyName, sourceInformationProvider, diagnosticMessageSink) => _serviceProvider = serviceProvider;
- 
+        public DiXunitTestFrameworkExecutor(IServiceProvider? serviceProvider, AssemblyName assemblyName,
+            ISourceInformationProvider sourceInformationProvider, IMessageSink diagnosticMessageSink)
+            : base(assemblyName, sourceInformationProvider, diagnosticMessageSink) =>
+            _serviceProvider = serviceProvider;
+
+        internal ExceptionAggregator Aggregator { get; set; } = new ExceptionAggregator();
+
         /// <inheritdoc />
         protected override async void RunTestCases(
             IEnumerable<IXunitTestCase> testCases,
             IMessageSink executionMessageSink,
             ITestFrameworkExecutionOptions executionOptions)
         {
-            if (_serviceProvider == null)
-                base.RunTestCases(testCases, executionMessageSink, executionOptions);
-            else
-            {
-                await new DiXunitTestAssemblyRunner(_serviceProvider, TestAssembly, testCases, DiagnosticMessageSink,
-                    executionMessageSink, executionOptions).RunAsync();
-            }
+            await new DiXunitTestAssemblyRunner(_serviceProvider, TestAssembly, testCases, DiagnosticMessageSink,
+                executionMessageSink, executionOptions, Aggregator).RunAsync();
         }
     }
 }
